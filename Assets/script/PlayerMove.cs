a4d0c5164966368e31954bf039844422a7210ef1
rio092 <raimugi8597@gmail.com>
2019-07-26 19:50:32 +0900

    移動だけ

---
+++ Assets/script/PlayerMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 右・左
+        float x = Input.GetAxisRaw("Horizontal");
+
+        // 上・下
+        float y = Input.GetAxisRaw("Vertical");
+        Vector2 direction = new Vector2(x, y), normalized;
+        GetComponent<Rigidbody2D>().velocity = direction * speed;
+        
+   }
+}
