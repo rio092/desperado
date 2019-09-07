e21e9179ced7e161b57c8feb831a1bf27b72ce9a
rio092 <raimugi8597@gmail.com>
2019-09-07 22:20:25 +0900

    no message

--- Assets/script/PlayerMove.cs
+++ Assets/script/PlayerMove.cs
@@ -5,22 +5,31 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float speed;
+    public float x;
+    public float y;
+
+    private Animator _anim;
+
+    public Animator Anim { get { return this._anim ? this._anim : this._anim = GetComponent<Animator>(); } }
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        // 右・左
-        float x = Input.GetAxisRaw("Horizontal");
+        Vector2 Position = transform.position;
+        // 右・左"Horizontal"
+        x = Input.GetAxisRaw("Horizontal");
+        //上・下"Vertical"
+        y = Input.GetAxisRaw("Vertical");
+        Vector2 direction = new Vector2(x,y).normalized;
+          GetComponent<Rigidbody2D>().velocity = direction;
 
-        // 上・下
-        float y = Input.GetAxisRaw("Vertical");
-        Vector2 direction = new Vector2(x, y), normalized;
-        GetComponent<Rigidbody2D>().velocity = direction * speed;
-        
-   }
+        Anim.SetFloat("x", x);
+        Anim.SetFloat("y", y);
+    }
+
 }
---
+++ Assets/script/Singleton.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour  where T:Singleton<T> 
+{
+    private static T singleton;
+    public static T Instance
+    {
+        get
+        {
+            return singleton;
+        }
+    }
+
+    protected void Awake()
+    {
+        if(singleton == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            singleton = (T)this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void DestroyMe()
+    {
+        singleton = null;
+        Destroy(gameObject);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
