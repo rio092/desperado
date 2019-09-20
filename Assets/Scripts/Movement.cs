2523829c1110308f63b7e9d2266663f398f9f399
rio092 <raimugi8597@gmail.com>
2019-09-20 22:40:32 +0900

    弾を打つ　移動

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public static float x;
+    public static float y;
+    public float speed;
+    private Animator _anim;
+    public Animator Anim { get { return this._anim ? this._anim : this._anim = GetComponent<Animator>(); } }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 右・左
+        x = Input.GetAxisRaw("Horizontal");
+        // 上・下
+        y = Input.GetAxisRaw("Vertical");
+        Vector2 direction = new Vector2(x, y);
+        GetComponent<Rigidbody2D>().velocity = direction * speed;
+        Anim.SetFloat("x", x);
+        Anim.SetFloat("y", y);
+    }
+}
---
+++ Assets/Scripts/Shotpoint.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shotpoint : MonoBehaviour
+{
+    public GameObject bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform mytransform = this.transform;
+        Vector3 localPos = mytransform.localPosition;
+        if (Movement.x == 0 && Movement.y == 0)
+        { }
+        else
+        {
+            localPos.x = (Movement.x / 10);
+            localPos.y = (Movement.y / 10);
+            mytransform.localPosition = localPos;
+        }
+        if (Movement.x == 0 && Movement.y == 1)
+        {
+            this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0);
+        }
+        if (Movement.x == -1 && Movement.y == 1)
+        {
+        this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 45);
+        }
+        if (Movement.x == -1 && Movement.y == 0)
+        {
+        this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 90);
+        }
+        if (Movement.x == -1 && Movement.y == -1)
+        {
+            this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 135);
+        }
+        if (Movement.x == 0 && Movement.y == -1)
+        {
+            this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 180);
+        }
+        if (Movement.x == 1 && Movement.y == -1)
+        {
+            this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 225);
+        }
+        if (Movement.x == 1 && Movement.y == 0)
+        {
+            this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 270);
+        }
+        if (Movement.x == 1 && Movement.y == 1)
+        {
+            this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 315);
+        }
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Instantiate(bullet, transform.position, transform.rotation);
+
+        }
+    }
+ }
