28999ff5dc98690abcdc8173372264163527e46c
rio092 <raimugi8597@gmail.com>
2019-09-26 19:19:00 +0900

    移動、弾（間隔なし）

--- Assets/Scripts/Shotpoint.cs
+++ Assets/Scripts/Shotpoint.cs
@@ -16,9 +16,7 @@ public class Shotpoint : MonoBehaviour
     {
         Transform mytransform = this.transform;
         Vector3 localPos = mytransform.localPosition;
-        if (Movement.x == 0 && Movement.y == 0)
-        { }
-        else
+        if (Movement.x != 0 || Movement.y != 0)
         {
             localPos.x = (Movement.x / 10);
             localPos.y = (Movement.y / 10);
@@ -55,11 +53,15 @@ public class Shotpoint : MonoBehaviour
         if (Movement.x == 1 && Movement.y == 1)
         {
             this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 315);
+            Debug.Log("111");
         }
         if (Input.GetKeyDown(KeyCode.Z))
         {
-            Instantiate(bullet, transform.position, transform.rotation);
-
+           Instantiate(bullet, transform.position, transform.rotation);
+          //  Instantiate(bullet, transform);
+            Debug.Log("aaa");
         }
     }
  }
+//ifをへらす　
+//
