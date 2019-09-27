4bd11e734fbd6ed9c90e0451bec0bd0e8f851477
rio092 <raimugi8597@gmail.com>
2019-09-27 18:37:48 +0900

    ミス

---
+++ Assets/Scripts/CharactorSelector.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharactorSelector : MonoBehaviour
+{
+    private bool isSelected;
+    private bool inputIsEnabled;
+    [SerializeField, Range(1, 4)] private int playerID;
+    private int maxPlayerNum;
+    private string controllerName;
+    private int selectedCharaType;
+    [SerializeField] Image CharactorImageUI;
+    [SerializeField] Sprite[] CharactorImage;
+    [SerializeField] private float inputInterval;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+
+    }
+    void Start()
+    {
+        isSelected = false;
+        inputIsEnabled = true;
+        controllerName = "Gamepad" + playerID + "_";
+        selectedCharaType = (int)PlayerType.None;
+        PlayerCharactorUpdate(selectedCharaType);
+        maxPlayerNum = PlayerData.Instance.MaxPlayerNum;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inputIsEnabled)
+        {
+            if (!isSelected)
+            {
+                //input keycode is used for debug
+                if (Input.GetButtonDown(controllerName + "Function0") || Input.GetKeyDown(KeyCode.M))
+                {
+                    isSelected = true;
+                    selectedCharaType = (int)PlayerType.Charactor1;
+                    PlayerCharactorUpdate(selectedCharaType);
+                    inputIsEnabled = false;
+                    StartCoroutine(WaitInputInterval());
+                }
+            }
+            else
+            {
+                if(Input.GetButtonDown(controllerName + "Function1") || Input.GetKeyDown(KeyCode.N))
+                {
+                    isSelected = false;
+                    selectedCharaType = (int)PlayerType.None;
+                    PlayerCharactorUpdate(selectedCharaType);
+                    inputIsEnabled = false;
+                    StartCoroutine(WaitInputInterval());
+                }
+                else
+                {
+                    //float axis = Input.GetAxisRaw(controllerName + "Y");
+                    //for debug
+                    float axis = Input.GetAxisRaw("Vertical");
+                    if(axis >= 1.0f)
+                    {
+                        selectedCharaType = (selectedCharaType % maxPlayerNum) + 1;
+                        PlayerCharactorUpdate(selectedCharaType);
+                        inputIsEnabled = false;
+                        StartCoroutine(WaitInputInterval());
+                    }
+                    else if(axis <= -1.0f)
+                    {
+                        selectedCharaType--;
+                        if (selectedCharaType <= 0) selectedCharaType = maxPlayerNum;
+                        PlayerCharactorUpdate(selectedCharaType);
+                        inputIsEnabled = false;
+                        StartCoroutine(WaitInputInterval());
+                    }
+                }
+            }
+        }
+    }
+
+    private void PlayerCharactorUpdate(int playerType)
+    {
+        switch (playerType)
+        {
+            case (int)PlayerType.None:
+                PlayerData.Instance.PlayerTypes[playerID - 1] = PlayerType.None;
+                break;
+            case (int)PlayerType.Charactor1:
+                PlayerData.Instance.PlayerTypes[playerID - 1] = PlayerType.Charactor1;
+                break;
+            case (int)PlayerType.Charactor2:
+                PlayerData.Instance.PlayerTypes[playerID - 1] = PlayerType.Charactor2;
+                break;
+            case (int)PlayerType.Charactor3:
+                PlayerData.Instance.PlayerTypes[playerID - 1] = PlayerType.Charactor3;
+                break;
+            case (int)PlayerType.Charactor4:
+                PlayerData.Instance.PlayerTypes[playerID - 1] = PlayerType.Charactor4;
+                break;
+        }
+        CharactorImageUI.sprite = CharactorImage[playerType];
+    }
+
+    IEnumerator WaitInputInterval()
+    {
+        yield return new WaitForSecondsRealtime(inputInterval);
+        inputIsEnabled = true;
+    }
+}
---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerType
+{
+    None,
+    Charactor1,
+    Charactor2,
+    Charactor3,
+    Charactor4,
+}
+
+public class PlayerData : Singleton<PlayerData>
+{
+    private const int maxPlayerNum = 4;
+    public int MaxPlayerNum
+    {
+        get
+        {
+            return maxPlayerNum;
+        }
+    }
+
+    public int participantsNum()
+    {
+        int num = 0;
+        foreach(PlayerType type in PlayerTypes)
+        {
+            if (type != PlayerType.None) num++;
+        }
+        return num;
+    }
+    public PlayerType[] PlayerTypes { get; set; }
+
+    private void PlayerTypesInit()
+    {
+        PlayerTypes = new PlayerType[MaxPlayerNum];
+        for(int i = 0; i< PlayerTypes.Length; i++)
+        {
+            PlayerTypes[i] = PlayerType.None;
+        }
+    }
+    public int[] PlayerScore { get; set; }
+
+    private void PlayerScoreInit()
+    {
+        PlayerScore = new int[MaxPlayerNum];
+        for(int i = 0; i < PlayerScore.Length; i++)
+        {
+            PlayerScore[i] = 0;
+        }
+    }
+
+    private new void Awake()
+    {
+        base.Awake();
+        PlayerTypesInit();
+        PlayerScoreInit();
+        PlayerScore = new int[4] { 13, 22, 13, 2 };
+        PlayerTypes = new PlayerType[4]
+        {
+            PlayerType.Charactor1,
+            PlayerType.Charactor1,
+            PlayerType.Charactor1,
+            PlayerType.None
+        };
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
